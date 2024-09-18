8c8a2ee0f06db01d2ef04ca99a6d24243aa09b85
Zercore <119522892+ZercoreX@users.noreply.github.com>
2024-09-18 04:00:32 +0300

    left nav - footer

--- docs/scripts/nav.js
+++ docs/scripts/nav.js
@@ -4,3 +4,26 @@ toggle.addEventListener("click", () => {
     nav.classList.toggle('show');
     toggle.classList.toggle('active');
 })
+
+// ===================================
+let left_nav_toggle = document.querySelector('.nav-toggle');
+let leftNavUl = document.querySelector('.left-nav-ul');
+
+left_nav_toggle.addEventListener("click", () => {
+    leftNavUl.classList.toggle('show');
+    left_nav_toggle.classList.toggle('active');
+})
+
+// ==================================
+let page_top = document.querySelector('.top-page');
+
+window.onscroll = function () {
+    this.scrollY >= 1000 ? page_top.classList.add("show") : page_top.classList.remove("show");
+}
+
+page_top.onclick = function() {
+    window.scrollTo({
+        top: 0,
+        // behavior: "smooth",
+    });
+}
